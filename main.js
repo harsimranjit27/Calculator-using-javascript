52c88de6bc305561913e7a6612699f6097b8cff9
harsimranjit27 <harsimranjit0300.cse19@chitkara.edu.in>
2021-06-24 14:43:31 +0530

    created main.js file

---
+++ main.js
@@ -0,0 +1,17 @@
+
+// const numberButtons = document.querySelectorAll('[data-number]');
+// const operatorButtons = document.querySelectorAll('[data-operator]');
+// const equalsButton = document.querySelector('[data-equal]');
+// const clearButton = document.querySelector('[data-clear]'); 
+// const inputDisplay = document.querySelector('[data-input]');
+
+// let screen = document.getElementById("#input-bar");
+// let buttons = document.querySelectorAll('button');
+// for (const item of buttons) {
+//     item.addEventListener('click',(e) =>{
+//         buttonText = e.target.innerText;
+//         console.log(buttonText);
+//     })
+// }
+
+ 
